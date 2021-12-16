0917720ef17cf2b29673530f4ea26672a6444aa3
qrrk <19731636+qrrk@users.noreply.github.com>
2021-12-16 23:53:38 +0300

    Open changelog via link in game description

--- scripts/Catapult.gd
+++ scripts/Catapult.gd
@@ -3,9 +3,9 @@ extends Node
 
 const _GAME_DESC = {
 	"dda":
-		"[b]Cataclysm: Dark Days Ahead[/b] is a turn-based survival game set in a post-apocalyptic world. Scavenge, explore, craft, build, farm, repair and modify vehicles, install bionics, mutate, defend against [color=#009900]zombies[/color] and countless other monstrosities — all in a limitless, procedurally generated world!",
+		"[b]Cataclysm: Dark Days Ahead[/b] is a turn-based survival game set in a post-apocalyptic world. Scavenge, explore, craft, build, farm, repair and modify vehicles, install bionics, mutate, defend against [color=#009900]zombies[/color] and countless other monstrosities — all in a limitless, procedurally generated world! [color=#3b93f7][url=CHANGELOG]View Changelog[/url][/color]",
 	"bn":
-		"[b]Cataclysm: Bright Nights[/b]. Reject pedantry, embrace [color=#ff3300]!!fun!![/color]. This fork takes the game back to its sci-fi roguelike roots and reverts many controversial changes by the DDA team (pockets, proficiencies, freezing, and [color=#3b93f7][url=https://github.com/cataclysmbnteam/Cataclysm-BN/wiki/Changes-so-far]more[/url][/color]). Special attention is paid to combat, game balance and pacing.",
+		"[b]Cataclysm: Bright Nights[/b]. Reject pedantry, embrace [color=#ff3300]!!fun!![/color]. This fork takes the game back to its sci-fi roguelike roots and reverts many controversial changes by the DDA team (pockets, proficiencies, freezing, and [color=#3b93f7][url=https://github.com/cataclysmbnteam/Cataclysm-BN/wiki/Changes-so-far]more[/url][/color]). Special attention is paid to combat, game balance and pacing. [color=#3b93f7][url=CHANGELOG]View Changelog[/url][/color]",
 }
 
 onready var _settings = $"/root/SettingsManager"
@@ -25,8 +25,7 @@ onready var _fshelper = $FSHelper
 onready var _installer = $ReleaseInstaller
 onready var _btn_install = $Main/Tabs/Game/BtnInstall
 onready var _btn_refresh = $Main/Tabs/Game/Builds/BtnRefresh
-onready var changelog = $Main/Tabs/Game/ChangelogDialog
-onready var _btn_changelog = $Main/Tabs/Game/BtnChangelog
+onready var _changelog = $Main/Tabs/Game/ChangelogDialog
 onready var _btn_game_dir = $Main/Tabs/Game/CurrentInstall/Build/GameDir
 onready var _btn_play = $Main/Tabs/Game/CurrentInstall/BtnPlay
 onready var _lst_builds = $Main/Tabs/Game/Builds/BuildsList
@@ -272,7 +271,10 @@ func _on_soundpack_operation_finished() -> void:
 
 func _on_Description_meta_clicked(meta) -> void:
 	
-	OS.shell_open(meta)
+	if meta == "CHANGELOG":
+		_changelog.open()
+	else:
+		OS.shell_open(meta)
 
 
 func _on_BtnRefresh_pressed() -> void:
@@ -304,11 +306,6 @@ func _on_BtnInstall_pressed() -> void:
 	var release = _releases.releases[_get_release_key()][index]
 	var update = _settings.read("game") in _inst_probe.probe_installed_games()
 	_installer.install_release(release, _settings.read("game"), update)
-
-
-func _on_BtnChangelog_pressed() -> void:
-	
-	changelog.open()
 
 
 func _get_release_key() -> String:
@@ -372,10 +369,8 @@ func apply_game_choice() -> void:
 			if channel == "stable":
 				_rbtn_stable.pressed = true
 				_btn_refresh.disabled = true
-				_btn_changelog.disabled = true
 			else:
 				_btn_refresh.disabled = false
-				_btn_changelog.disabled = false
 				
 		"bn":
 			_lst_games.select(1)
