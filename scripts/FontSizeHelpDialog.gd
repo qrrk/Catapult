5e55867226ae063fab8851f6462a253265180b8f
qrrk <19731636+qrrk@users.noreply.github.com>
2022-04-25 22:30:08 +0300

    Scale pictures in font help dialog

--- scripts/FontSizeHelpDialog.gd
+++ scripts/FontSizeHelpDialog.gd
@@ -6,7 +6,10 @@ onready var _label := $Panel/Margin/VBox/Help
 
 func open() -> void:
 	
-	_label.bbcode_text = tr("dlg_font_config_help")
+	var text := tr("dlg_font_config_help")
+	text = text.replace("[IMG_1]", "[img=%d]res://images/font-sizes.png[/img]" % (180 * Geom.scale))
+	text = text.replace("[IMG_2]", "[img=%d]res://images/font-rect.png[/img]" % (400 * Geom.scale))
+	_label.bbcode_text = text
 	_label.scroll_to_line(0)
 #
 	popup_centered_ratio(0.9)
