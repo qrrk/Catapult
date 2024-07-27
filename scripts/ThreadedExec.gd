edb4e2172019ed75e64a552d5e5506e4935cbef0
qrrk <19731636+qrrk@users.noreply.github.com>
2024-07-27 21:21:20 +0100

    Simplify code for threaded function calls

--- scripts/ThreadedExec.gd
+++ scripts/ThreadedExec.gd
@@ -4,22 +4,8 @@ extends Node
 
 signal execution_finished
 
-#var _worker: Thread
 var output := []
 var last_exit_code := 0
-
-
-#func _wrapper(path_and_args: Array) -> void:
-	#pass # FIXME
-	#exit_code = OS.execute(path_and_args[0], path_and_args[1], true, output, true)
-	#emit_signal("process_exited")
-	#_worker.call_deferred("wait_to_finish")
-
-#func execute(path: String, args: PackedStringArray) -> void:
-	#
-	#pass # FIXME
-	#_worker = Thread.new()
-	#_worker.start(Callable(self, "_wrapper").bind([path, args]))
 
 
 func execute(path: String, args: PackedStringArray) -> void:
@@ -30,6 +16,3 @@ func execute(path: String, args: PackedStringArray) -> void:
 		await get_tree().process_frame
 	last_exit_code = thread.wait_to_finish()
 	execution_finished.emit()
-	
-	
-	
