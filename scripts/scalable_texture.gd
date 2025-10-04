4cb736becd7f85869657cf41ff4785c9aa28010f
qrrk <19731636+qrrk@users.noreply.github.com>
2025-10-04 22:34:05 +0100

    Fix scaling of ScalableImageTexture

--- scripts/scalable_texture.gd
+++ scripts/scalable_texture.gd
@@ -2,16 +2,14 @@ class_name ScalableImageTexture
 extends ImageTexture
 
 
-var _base_size: Vector2
+@export var base_display_size: Vector2 = Vector2(16, 16)
 
 
 func _init() -> void:
 	
-	pass  # FIXME
-	#_base_size = self.get_size()
-	#Geom.connect("scale_changed", Callable(self, "_on_ui_scale_changed"))
+	Geom.connect("scale_changed", Callable(self, "_on_ui_scale_changed"))
 
 
 func _on_ui_scale_changed(new_scale: float) -> void:
 	
-	self.set_size_override(_base_size * new_scale)
+	self.set_size_override(base_display_size * new_scale)
