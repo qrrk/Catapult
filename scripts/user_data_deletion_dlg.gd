d5ff77675458efecbae4b9190e9b21949d072857
qrrk <19731636+qrrk@users.noreply.github.com>
2025-11-09 17:11:46 +0000

    De-hardcode user data deletion dialog text

--- scripts/user_data_deletion_dlg.gd
+++ scripts/user_data_deletion_dlg.gd
@@ -18,20 +18,15 @@ func open(folders_list: Array[String]) -> void:
 	
 	%SafetyField.clear()
 	%WarningText.clear()
-	%WarningText.append_text("Stop!\
-	Misplaced user data was detected in the game directory.\
-	This could happen if you ran the game directly from its executable\
-	instead of using Catapult. This is not supported, and if you proceed,\
-	all that data will be deleted irrecoverably.[br][br]\
-	User data was found in the following folders:[br]")
+	%WarningText.append_text(tr("dlg_userdata_deletion_text_pt1"))
 	
 	%WarningText.push_list(0, RichTextLabel.LIST_DOTS, false)
 	for path in folders_list:
 		%WarningText.append_text("[url]%s[/url]\n" % path)
 	%WarningText.pop_all()
-	%WarningText.newline()
 	
-	%WarningText.append_text("Review these folders and back up anything important before confirming.")
+	%WarningText.newline()
+	%WarningText.append_text(tr("dlg_userdata_deletion_text_pt2"))
 	
 	popup_centered()
 
